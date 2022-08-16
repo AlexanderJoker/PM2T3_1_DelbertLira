94a5338117ccfe227d3ca4e95d94ce5da6716e57
AlexanderJoker <alexandertinoco12349@gmail.com>
2022-08-15 23:30:37 -0600

    Add project files.

---
+++ PM2T3_1_DelbertLira/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2T3_1_DelbertLira
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
